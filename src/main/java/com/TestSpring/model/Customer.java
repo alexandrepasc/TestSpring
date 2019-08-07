3207a5c30b3697c2e1ea29475df91c8ebd17c67b
alexandrepasc <alexandre.pascoal@gmail.com>
2019-08-07 18:27:19 +0100

    Add repository and model
    Add repo and model classes.
    Add some documentation.

---
+++ src/main/java/com/TestSpring/model/Customer.java
@@ -0,0 +1,48 @@
+package com.TestSpring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "firstname")
+  private String firstName;
+
+  @Column(name = "lastname")
+  private String lastName;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
---
+++ src/main/java/com/TestSpring/repository/CustomerRepo.java
@@ -0,0 +1,14 @@
+package com.TestSpring.repository;
+
+import com.TestSpring.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
+
+  List<Customer> findByFirstName(String FirstName);
+  List<Customer> findAll();
+}
